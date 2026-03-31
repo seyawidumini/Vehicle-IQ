d76d9fc94d90cf198fd60921d19acfdb9d72fc01
Ramitha <it24101559@my.sliit.lk>
2026-03-31 12:46:44 +0530

    user

--- main/__init__.py
+++ main/__init__.py
@@ -22,26 +22,9 @@ def load_user(user_id):
         return admin
     return User.query.get(int(user_id))
 
-with open("main/artifacts/model.pkl", "rb") as f:
-    model = pickle.load(f)
-
-with open("main/artifacts/model_columns.pkl", "rb") as f:
-    model_columns = pickle.load(f)
-
 from main.Users.routes import users
-from main.About.routes import info
-from main.Feedbacks.routes import feedbacks
 from main.Home.routes import index
-from main.Predictions.routes import prediction
-from main.Search.routes import searchs
-from main.Admins.routes import admin
-from main.Error.handler import errors
+
 
 app.register_blueprint(users)
-app.register_blueprint(info)
-app.register_blueprint(feedbacks)
 app.register_blueprint(index)
-app.register_blueprint(prediction)
-app.register_blueprint(searchs)
-app.register_blueprint(admin)
-app.register_blueprint(errors)
